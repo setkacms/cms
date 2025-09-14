fbbfe2f496a10acabe08fe756aab793b608307c0
Vitaliy Kamelin <v.kamelin@gmail.com>
2025-09-14 23:48:10 +0500

    chore(deps): update aws/aws-sdk-php to ^3.356, league/flysystem-aws-s3-v3 to ^1.0

---
+++ src/Console/Module.php
@@ -0,0 +1,25 @@
+<?php
+/*
+ * This file is part of Setka CMS.
+ *
+ * Copyright (c) 2025 Vitaliy Kamelin. All rights reserved.
+ * Proprietary license. Unauthorized copying, modification or distribution
+ * of this file, via any medium, is strictly prohibited without prior written permission.
+ *
+ * @package   Setka CMS
+ * @version   1.0.0
+ * @author    Vitaliy Kamelin <v.kamelin@gmail.com>
+ * @license   Proprietary
+ *
+ * https://github.com/setkacms/cms
+ * See LICENSE file for details.
+ */
+
+namespace Setka\Cms\Console;
+
+use yii\base\Module as BaseModule;
+
+final class Module extends BaseModule
+{
+    public $controllerNamespace = 'Setka\\Cms\\Console\\Controllers';
+}

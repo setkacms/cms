7428ab93bca4cdd6efbe18f12151e8fbb8bb9631
Vitaliy Kamelin <v.kamelin@gmail.com>
2025-09-16 04:33:55 +0500

    feat(ui): add dashboard layout and views

--- src/Http/Dashboard/Module.php
+++ src/Http/Dashboard/Module.php
@@ -12,5 +12,15 @@ use yii\base\Module as BaseModule;
 class Module extends BaseModule
 {
     public $controllerNamespace = 'Setka\\Cms\\Http\\Dashboard\\Controllers';
+
+    public function init(): void
+    {
+        parent::init();
+
+        $this->setViewPath(__DIR__ . DIRECTORY_SEPARATOR . 'Views');
+        $this->setLayoutPath($this->getViewPath() . DIRECTORY_SEPARATOR . 'layouts');
+        $this->layout = 'main';
+    }
 }
+
 
